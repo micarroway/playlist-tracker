4bc70c8876d98c45bc1a61b36c096488ad16ba8c
jurobn <jurobn@amazon.com>
2017-06-27 09:50:57 -0400

    [master] comments

--- stats.py
+++ stats.py
@@ -17,6 +17,9 @@ with open('spotify_config.json') as config:
 token = util.prompt_for_user_token(**config['oath'])
 sp = spotipy.Spotify(auth=token)
 
+# JUROBN You need a User class to represent each person. I would also suggest a Users class to hold them all.
+# JUROBN Good use of dict for quick lookup. Do you know how dict/hashmaps work under the hood?
+# JUROBN The names are fine in this small scope but in real code you don't to hard code names like this. I would see if could programatically pull them in, if not just show the username
 # could take the trouble to create and update this in real time...
 USERS = {'1277120721': {'person': 'Jaki',
                         'popularity': [],
@@ -55,20 +58,29 @@ SONGS = []
 
 
 def ms_to_min(ms):
+    # JUROBN why not just one math operation? It's a small inefficiency but where do you draw the line?
     x = ms / 1000
     minutes = x / 60
     return(round(minutes, 2))
 
 
 def is_new_id(test):
+    # JUROBN you can just `return test not in UNIQUE_IDS`
+    # JUROBN Is UNIQUE_IDS a dictionary or list? If it's a list this is O(n) time complexity, dictionary would reduce to O(1)
+    # JUROBN UNIQUE_IDS is an ambiguous variable name even in this narrow spotify scope.
+    #   ID of what? Track? User? Playlist? Something you created?
     if test in UNIQUE_IDS:
         return(False)
     else:
         return(True)
 
 
+# JUROBN get what data? I can't tell what this method does without looking at the code.
+#   I would suggest populate_user_stats_from_tracks since you dont actual return data
 def get_data(tracks):
     for i, item in enumerate(tracks['items']):
+        # JUROBN I'm not familiar with python common practices with local variables names.
+        # Do they want you to underscore them or is this just something you like?
         _added_by = item['added_by']['id']
         if is_new_id(_added_by):
             UNIQUE_IDS.append(_added_by)
@@ -80,15 +92,19 @@ def get_data(tracks):
         USERS[_added_by]['popularity'].append(_pop)
     return(total)
 
-
+# JUROBN define "clean" data. better method names
 def collect_clean_data():
     data = []
     for _id in UNIQUE_IDS:
         _person = USERS[_id]['person']
         _song_count = str(USERS[_id]['song_count'])
         _total_time = str(ms_to_min(USERS[_id]['total_time']))
+        # JUROBN good job not keeping a running average. However, you don't need to store all the individual popularity ratings.
+        #   You just need to store the sum so far and the count so far. This will reduce time and space complexity from O(n) to O(1)
+        #   it would also eliminate the need for your AVG_POP variable
         _avg = sum(USERS[_id]['popularity']) / len(USERS[_id]['popularity'])
         _pop = str(round(_avg, 2))
+        # JUROBN why store the popularities in their own variable if you have the data already?
         for score in USERS[_id]['popularity']:
             AVG_POP.append(score)
         data.append([_person, _song_count, _total_time, _pop])
@@ -109,6 +125,7 @@ def average_popularity():
 
 
 def is_time_over(total_time, data):
+    # JUROBN where does this number come from? I know it's calculated from car travel but how would someone else know? Maybe put in config.
     _allowed_minutes = 1440
     _limit = round(_allowed_minutes - total_time)
     _per_person_limit = _allowed_minutes / 7
@@ -121,6 +138,7 @@ def is_time_over(total_time, data):
         print('The total time is under the limit by %s mins.' % _limit)
 
     for person in data:
+        # JUROBN the fuck? person[2]?
         _personal_time = float(person[2])
         if _personal_time > _per_person_limit:
             _overage = str(round(_personal_time - _per_person_limit))
@@ -135,6 +153,7 @@ def is_time_over(total_time, data):
 def pprint(data):
     print()
     for person in data:
+        # JUROBN what do these indices mean? You should make a class that holds the properties you are trying to show
             print('%s: %s songs, %s mins, %s Avg Popularity' % (person[0].rjust(8),
                                                                 person[1].rjust(3),
                                                                 person[2].rjust(6),
@@ -160,6 +179,7 @@ if __name__ == '__main__':
         tracks = sp.next(tracks)
         get_data(tracks)
 
+    # JUROBN do you know the time complexity of the average sorting algorithm? It's a decent amount of overhead.
     sort_by = {'person': 0,
                'songs': 1,
                'time': 2,
