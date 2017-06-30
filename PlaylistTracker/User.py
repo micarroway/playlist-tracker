0f879ad2c752a05f87c8a58751af9b6d21e8124a
catoejr <catoejr@gmail.com>
2017-06-30 18:36:02 -0500

    added genres list

--- PlaylistTracker/User.py
+++ PlaylistTracker/User.py
@@ -4,13 +4,15 @@ from PlaylistTracker.SpotifyClient import SpotifyClient
 class User:
     sp = SpotifyClient.get_client()
 
-    __slots__ = ['username', 'popularity', 'song_count', 'total_seconds', '_spotify_profile', 'display_name']
+    __slots__ = ['username', 'popularity', 'song_count', 'total_seconds',
+                 'artist', 'genres', '_spotify_profile', 'display_name']
 
     def __init__(self, username):
         self.username = username
         self.popularity = 0
         self.song_count = 0
         self.total_seconds = 0
+        self.genres = []
         self._spotify_profile = self.sp.user(self.username)
         self.display_name = self._spotify_profile['display_name'] or self._spotify_profile['id']
 
@@ -26,10 +28,22 @@ class User:
     def get_total_minutes(self):
         return self.total_seconds / 60000
 
+    def get_genres(self, artist):
+        artist_genres = []
+        results = self.sp.search(q='artist:' + artist, type='artist')
+        for result in results['artists']['items']:
+            if result['genres']:
+                for genre in result['genres']:
+                    if genre not in artist_genres:
+                        artist_genres.append(genre)
+        return(artist_genres)
+
     def add_track(self, track):
         self.popularity += track['popularity']
         self.song_count += 1
         self.total_seconds += track['duration_ms']
+        self.artist = track['artists'][0]['name']
+        self.genres.append(self.get_genres(self.artist))
 
     def get_average_popularity(self):
         if not self.song_count:
