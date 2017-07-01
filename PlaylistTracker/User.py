70c90dc75aeba971f99cbfb45496a14f3ea61a3e
catoejr <catoejr@gmail.com>
2017-07-01 14:48:29 -0500

    removed genres so tests pass

--- PlaylistTracker/User.py
+++ PlaylistTracker/User.py
@@ -5,19 +5,19 @@ class User:
     sp = SpotifyClient.get_client()
 
     __slots__ = ['username', 'popularity', 'song_count', 'total_seconds',
-                 'artist', 'genres', '_spotify_profile', 'display_name']
+                 '_spotify_profile', 'display_name']
 
     def __init__(self, username):
         self.username = username
         self.popularity = 0
         self.song_count = 0
         self.total_seconds = 0
-        self.genres = []
         self._spotify_profile = self.sp.user(self.username)
         self.display_name = self._spotify_profile['display_name'] or self._spotify_profile['id']
 
     def __repr__(self):
-        return repr((self.display_name, self.song_count, self.total_seconds, self.get_average_popularity()))
+        return repr((self.display_name, self.song_count, self.total_seconds,
+                     self.get_average_popularity()))
 
     def __str__(self):
         return '%s: %s songs, %s mins, %s Avg Popularity' % (self.display_name.rjust(20),
@@ -28,22 +28,10 @@ class User:
     def get_total_minutes(self):
         return self.total_seconds / 60000
 
-    def get_genres(self, artist):
-        artist_genres = []
-        results = self.sp.search(q='artist:' + artist, type='artist')
-        for result in results['artists']['items']:
-            if result['genres']:
-                for genre in result['genres']:
-                    if genre not in artist_genres:
-                        artist_genres.append(genre)
-        return(artist_genres)
-
     def add_track(self, track):
         self.popularity += track['popularity']
         self.song_count += 1
         self.total_seconds += track['duration_ms']
-        self.artist = track['artists'][0]['name']
-        self.genres.append(self.get_genres(self.artist))
 
     def get_average_popularity(self):
         if not self.song_count:
