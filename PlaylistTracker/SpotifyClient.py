c5c70b404097538bc3078cbcbf78965d4ceb915e
jurobn <jurobn@amazon.com>
2017-07-03 16:00:53 -0700

    [master] one global spotify client

--- PlaylistTracker/SpotifyClient.py
+++ PlaylistTracker/SpotifyClient.py
@@ -5,21 +5,23 @@ from .AppConfig import AppConfig
 
 
 class SpotifyClient:
+    __oath = AppConfig.config['oath']
+    client = spotipy.Spotify(
+        client_credentials_manager=SpotifyClientCredentials(
+            client_id=__oath['client_id'],
+            client_secret=__oath['client_secret']
+        )
+    )
 
     """
     static methods are methods we can call with creating a new SpotifyClient object
-    this will create a new spotipy client every time you run SpotipyClient.get_client()
+    this will return the same spotipy client every time you run SpotipyClient.get_client()
     """
+
     @staticmethod
     def get_client():
         """
-        Returns a new instance of a spotipy client with our credentials
+        Returns a global instance of a spotipy client with our credentials
         :return spotipy.Spotify instance
         """
-        oath = AppConfig.config['oath']
-        return spotipy.Spotify(
-            client_credentials_manager=SpotifyClientCredentials(
-                client_id=oath['client_id'],
-                client_secret=oath['client_secret']
-            )
-        )
+        return SpotifyClient.client
